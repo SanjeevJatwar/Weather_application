4dd4a043f278726e518b5b592cba86c07087a0e8
unknown <jatwarsanjeev@gmail.com>
2026-07-11 18:25:47 +0530

    Initials

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Weather_application
-Using React, Fastapi
+Using React I build naive weather application where you can search for the city and get the weather info.
+
+this is lovely because :
+I learned This is a huge milestone because you combined:
+ useState Controlled Inputs fetch() async/await Conditional Rendering API Integration State Update
--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,34 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from './assets/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const api_key = import.meta.env.VITE_API_KEY
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError]  =  useState("");
+  
+  async function getWeather() {
+    if(!city){setError("Enter city name");}
 
-  return (
-    <div className="App">
+    setLoading(true);
+    const res= await fetch(
+ `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
+    const data = await res.json();
+    if (data.cod !== 200) {
+    setError(data.message);
+    setWeather(null);
+    setLoading(false);
+    return;
+}
+    setError("");
+    
+    setWeather(data)
+    setLoading(false);
+  }
+
+  return(
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1> Weather Application</h1>
+        <input
+          type = "text"
+          placeholder='Enter city'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => {if(e.key == "Enter") getWeather();}}
+          />
+          <button onClick={getWeather}>Search</button>
+          {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          
+          <h2>{weather.main.temp} °C</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>🌡 Temperature: {weather.main.temp} °C</p>
+<p>🤒 Feels Like: {weather.main.feels_like} °C</p>
+<p>💧 Humidity: {weather.main.humidity}%</p>
+<p>🌬 Wind: {weather.wind.speed} m/s</p>
+<p>🌍 Country: {weather.sys.country}</p>
+        </div>
+      )}
+          
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    );
 }
 
 export default App
